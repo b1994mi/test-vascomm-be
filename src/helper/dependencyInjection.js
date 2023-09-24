00c86ce8239316dad30a722361c11b8bad9bfed1
b1994mi <b1994mi@gmail.com>
2023-09-24 20:45:03 +0700

    add dependency injection

--- src/helper/dependencyInjection.js
+++ src/helper/dependencyInjection.js
@@ -1,7 +1,13 @@
+const uuid = require('uuid')
 const db = require('../model')
+const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
 
 const DI = {
+    uuid,
     db,
+    bcrypt,
+    jwt,
 }
 
 module.exports = DI

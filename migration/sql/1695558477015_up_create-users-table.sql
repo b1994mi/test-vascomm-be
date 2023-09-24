b964d5f780b3d33c3be31611db9b907d1fa3cb79
b1994mi <b1994mi@gmail.com>
2023-09-24 20:57:14 +0700

    add role id col to users

--- migration/sql/1695558477015_up_create-users-table.sql
+++ migration/sql/1695558477015_up_create-users-table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE public.users
 (
     id bigserial,
+    role_id int NOT NULL DEFAULT 2,
     email character varying(320) NOT NULL,
     password character varying(258) NOT NULL,
     phone_number character varying(14) NOT NULL,
